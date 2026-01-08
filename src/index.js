c3df27657c516aee34b1d2b41df7d0f6db87aed6
jpwork-16 <jaiprakash6c@gmail.com>
2026-01-08 16:23:10 +0530

    Refactor imports and update console output messages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const { normalizeTrialBalance } = require("./engine/normalize");
-const { computeProfitAndLoss } = require("./engine/compute");
+const {
+  computeProfitAndLoss,
+  updateCapital
+} = require("./engine/compute");
+
 const input = require("../data/input.json");
 
 const accounts = normalizeTrialBalance(input.trialBalance);
 const pl = computeProfitAndLoss(accounts);
 
+updateCapital(accounts, pl.profit);
+
 console.log("PROFIT & LOSS");
 console.log(pl);
 
-console.log("\nWORKINGS");
+console.log("\nFINAL ACCOUNTS WITH WORKINGS");
 console.log(JSON.stringify(accounts, null, 2));
