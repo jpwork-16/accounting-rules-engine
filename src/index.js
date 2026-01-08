2b47bd0726a2002faee0cbfd4f4ff706f135e859
jpwork-16 <jaiprakash6c@gmail.com>
2026-01-08 16:10:30 +0530

    Add profit and loss computation to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const { normalizeTrialBalance } = require("./engine/normalize");
+const { computeProfitAndLoss } = require("./engine/compute");
 const input = require("../data/input.json");
 
 const accounts = normalizeTrialBalance(input.trialBalance);
+const pl = computeProfitAndLoss(accounts);
 
-console.log(accounts);
+console.log("PROFIT & LOSS");
+console.log(pl);
