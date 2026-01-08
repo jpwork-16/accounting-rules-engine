bf6339353977e4f812b9071e04a9a0c66d9f14d9
jpwork-16 <jaiprakash6c@gmail.com>
2026-01-08 15:57:18 +0530

    Added real code (V1)

--- src/engine/normalize.js
+++ src/engine/normalize.js
@@ -1,2 +1,18 @@
-// Normalization logic will go here
+function normalizeTrialBalance(trialBalance) {
+  const normalizedAccounts = {};
 
+  for (const accountName in trialBalance) {
+    const account = trialBalance[accountName];
+
+    normalizedAccounts[accountName] = {
+      name: accountName,
+      category: account.type,
+      balance: account.balance,
+      history: []
+    };
+  }
+
+  return normalizedAccounts;
+}
+
+module.exports = { normalizeTrialBalance };
