00b739dc3305bec0fa054ea0b40007164a90e919
jpwork-16 <jaiprakash6c@gmail.com>
2026-01-08 16:09:01 +0530

    Add computeProfitAndLoss function for accounts

--- src/engine/compute.js
+++ src/engine/compute.js
@@ -1,1 +1,26 @@
+function computeProfitAndLoss(accounts) {
+  let totalIncome = 0;
+  let totalExpenses = 0;
 
+  for (const accountName in accounts) {
+    const account = accounts[accountName];
+
+    if (account.category === "Income") {
+      totalIncome += account.balance;
+    }
+
+    if (account.category === "Expense") {
+      totalExpenses += account.balance;
+    }
+  }
+
+  const profit = totalIncome - totalExpenses;
+
+  return {
+    totalIncome,
+    totalExpenses,
+    profit
+  };
+}
+
+module.exports = { computeProfitAndLoss };
